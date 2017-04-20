93f0ab846c3583934277910c8eeb3ce35c5a2c9b
Taylor McKay <taylormckay68@gmail.com>
2017-04-20 13:12:20 -0600

    finished project

---
+++ app.js
@@ -0,0 +1,46 @@
+$(document).ready(function() {
+  var colors = 'white green red blue yellow';
+
+  $('.box').on('click', function() {
+    $(this).addClass('white');
+  })
+  $('.box').on('dblclick', function() {
+    $(this).removeClass('white');
+  })
+  $('#reset').on('click', function() {
+    $('.box').removeClass('white');
+  })
+
+  $('#red').on('click', function() {
+    color = 'red';
+  })
+  $('#blue').on('click', function() {
+    color = 'blue';
+  })
+
+  $('#green').on('click', function() {
+    color = 'green';
+  })
+
+  $('#yellow').on('click', function() {
+    color = 'yellow';
+  })
+
+  $('#white').on('click', function() {
+    color = 'white';
+  })
+  $('.box').on('click', function() {
+    $(this).addClass(color);
+  })
+  $('.box').on('dblclick', function() {
+    $(this).removeClass(color);
+  })
+
+  $('#reset').on('click', function() {
+    $('.box').removeClass(color)
+  })
+
+
+
+
+});
